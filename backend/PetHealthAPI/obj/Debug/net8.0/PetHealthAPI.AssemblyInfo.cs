0e8e69830cad9adda28eae8c367dc73169df9cc8
Murial Anindo <murial.anindo@lonett.com>
2025-08-07 12:40:00 -0400

    changes

--- backend/PetHealthAPI/obj/Debug/net8.0/PetHealthAPI.AssemblyInfo.cs
+++ backend/PetHealthAPI/obj/Debug/net8.0/PetHealthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetHealthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781e9cbbd20c78713bd97d27aeb2d3674ee81ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1552629734651aa50e6ce85d347293b75d9810b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetHealthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetHealthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
