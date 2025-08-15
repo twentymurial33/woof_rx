5adf6b4e5fd07cb2560737a0503ec7289755f474
Murial Anindo <murial.anindo@lonett.com>
2025-08-15 10:06:17 -0400

    changes for footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Main />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,10 +1,13 @@
 import Header from './Header';
+import Footer from './Footer';
 
 
 export default function Main() {
   return (
     <div>
       <Header />
+      <main></main>
+      <Footer />
     </div>
   );
 }
